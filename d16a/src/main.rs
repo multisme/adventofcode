5021308ee95025a39c697d50c87237c51cf4557b
multi prise <korokoko.toi@gmail.com>
2025-12-01 11:15:54 +0100

    d16a

--- d16a/src/main.rs
+++ d16a/src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{VecDeque};
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug)]
 struct Position {
     x: i32,
     y: i32,
 }
 
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug)]
 struct Score {
     char: char,
     points: i32,
@@ -60,46 +60,34 @@ fn parse_grid(map_str: &str) -> (usize, usize, Position, Position, Vec<Vec<Score
     (width, height, start, end, grid)
 }
 
-fn print_grid(width: usize, height: usize, grid: HashMap<Position, Score>) {
-    let mut map = vec![vec![0; width + 1]; height + 1];
-    grid.iter().for_each(|(p, c)| {
-        map[p.y as usize][p.x as usize] = c.points;
-    });
-    map.iter().for_each(|l| println!("{:05?}", l));
-    println!("\n\n");
-}
-
-fn map_grid(grid: &mut Vec<Vec<Score>>, pos: Position, direction: Direction, points: i32) {
-    match grid[pos.y as usize][pos.x as usize] {
-        score @ Score { char: '.', .. } | score @ Score { char: 'S', .. } => {
-            if score.points == 0 || score.points > points {
-                grid[pos.y as usize][pos.x as usize].points = points; 
-                let (v_inc, h_inc) = match direction {
-                    Direction::Vertical => (1, 1001),
-                    Direction::Horizontal => (1001, 1),
-                };
-                map_grid(grid, pos.top(), Direction::Vertical, points + v_inc);
-                map_grid(grid, pos.bottom(), Direction::Vertical, points + v_inc);
-                map_grid(grid, pos.left(), Direction::Horizontal, points + h_inc);
-                map_grid(grid, pos.right(), Direction::Horizontal, points + h_inc);
-            }
-        }
-        score @ Score { char: 'E', .. } => {
-            if score.points == 0 || score.points > points {
-                grid[pos.y as usize][pos.x as usize].points = points; 
-            }
-        }
-        _ => (),
-    }
-}
-
 fn main() {
     let mut data = include_str!("../input.txt").split("\n\n");
     let (_width, _height, start, end, mut grid) = parse_grid(data.next().unwrap());
-    let mut deque: VecDeque<Position> = VecDeque::new();
-    deque.push_back(start);
-    map_grid(&mut grid, start, Direction::Horizontal, 0);
-    //print_grid(width, height, grid.clone());
+    let mut deque: VecDeque<(Position, Direction, i32)> = VecDeque::new();
+    deque.push_back((start, Direction::Horizontal, 1));
+    while let Some((pos, direction, points)) =  deque.pop_back(){
+        match &grid[pos.y as usize][pos.x as usize] {
+            score @ Score { char: '.', .. } | score @ Score { char: 'S', .. } => {
+                if score.points == 0 || score.points > points {
+                    grid[pos.y as usize][pos.x as usize].points = points; 
+                    let (v_inc, h_inc) = match direction {
+                        Direction::Vertical => (1, 1001),
+                        Direction::Horizontal => (1001, 1),
+                    };
+                    deque.push_back((pos.top(), Direction::Vertical, points + v_inc));
+                    deque.push_back((pos.bottom(), Direction::Vertical, points + v_inc));
+                    deque.push_back((pos.right(), Direction::Horizontal, points + h_inc));
+                    deque.push_back((pos.left(), Direction::Horizontal, points + h_inc));
+                }
+            }
+            score @ Score { char: 'E', .. } => {
+                if score.points == 0 || score.points > points {
+                    grid[pos.y as usize][pos.x as usize].points = points; 
+                }
+            }
+            _ => (),
+        }
+    }
     let res = grid[end.y as usize][end.x as usize].points;
     println!("{res}");
 }
