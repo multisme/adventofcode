15e5221732a8f03ba1eca80b5c8cbe82132366b5
multi prise <korokoko.toi@gmail.com>
2025-12-07 19:20:39 +0100

    d05b corrected

--- 2025/d05b/src/main.rs
+++ 2025/d05b/src/main.rs
@@ -1,12 +1,10 @@
-use std::collections::HashSet;
-
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 enum LimitType{
     Start,
     End
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 struct Limit{
     value: i64,
     limittype: LimitType
@@ -24,10 +22,9 @@ fn parse_data(data: &'static str) -> Vec<Limit> {
             let start = limits.next().unwrap().parse::<i64>().unwrap();
             let end = limits.next().unwrap().parse::<i64>().unwrap();
             limits_vec.push(Limit{value: start, limittype: LimitType::Start});
-            limits_vec.push(Limit{value: end + 1, limittype: LimitType::End})
+            limits_vec.push(Limit{value: end, limittype: LimitType::End})
         });
     limits_vec.sort_by(|a, b| {a.value.cmp(&b.value)});
-    //println!("{:?}", limits_vec);
     limits_vec
 }
 
@@ -36,33 +33,30 @@ fn count_fresh(limits: Vec<Limit>) -> i64 {
     let mut start = Limit{value: 0, limittype: LimitType::Start};
     let mut end = Limit{value: 0, limittype: LimitType::End};
     let mut overlaps = 0;
-    for limit in limits {
+    for limit in &limits{
         match limit.limittype{
             LimitType::Start => {
                 if overlaps == 0{
-                    start = limit;
-                    if end.value == start.value {
-                        start.value += 1;
-                    }
+                    start = *limit;
                 }
                 overlaps +=1;
+                if end.value == start.value {
+                    start.value += 1;
+                }
             }
             LimitType::End=> {
                 overlaps -=1;
                 if overlaps == 0{
-                    if start.value == limit.value{
-                        println!("ahahah {} {}", start.value, limit.value);
-                    }
-                    result += limit.value - start.value;
-                    end = limit;
+                    result += limit.value - start.value + 1;
                 }
+                end = *limit;
             }
         }
         if overlaps < 0 {
             println!("bug");
         }
     }
-    result
+    result 
 }
 
 fn main() {
@@ -75,8 +69,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_second_example() {
         let ranges = parse_data("3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32\n");
-        assert_eq!(14, count_fresh(ranges))
+        assert_eq!(14, count_fresh(ranges));
+    }
+
+    #[test]
+    fn test_example() {
+        let ranges = parse_data("3-5\n10-14\n16-18\n12-18\n18-19\n18-18\n\n1\n5\n8\n11\n17\n32\n");
+        assert_eq!(13, count_fresh(ranges));
     }
 }
